c315c1953786ed3e58fa049267f538d766c8fcae
Arthur Zagretdinov <arthur.zagretdinov@outlook.com>
2017-09-25 14:46:09 +0200

    Mirror tree from geekforgeek

--- geekforgeek/MirrorTree.h
+++ geekforgeek/MirrorTree.h
@@ -2,12 +2,16 @@
 // Created by Arthur Zagretdinov on 23/09/2017.
 //
 
+#include "tree.h"
+
 #ifndef CODINGTASKS_MIRRORTREE_H
 #define CODINGTASKS_MIRRORTREE_H
 
 
-class MirrorTree {
 
+class MirrorTree {
+public:
+    void mirror(Node* node);
 };
 
 
--- main.cpp
+++ main.cpp
@@ -1,70 +1,36 @@
 #include <cmath>
 #include <vector>
 #include <iostream>
-#include "leetcode/SolutionSearch2DMatrix.h"
+#include "geekforgeek/tree.h"
+#include "geekforgeek/MirrorTree.h"
+
+void run_tc(string tree) {
+    Node *root = parse_string_to_tree(&tree);
+    
+    cout << "In: ";
+    print_tree(root);
+    
+    
+    MirrorTree task;
+    task.mirror(root);
+    
+    cout << " Out : ";
+    print_tree(root);
+    cout << endl;
+}
 
 using namespace std;
 
 int main() {
-    SolutionSearch2DMatrix s;
-    vector<vector<int>> empty (0);
-    
-    cout << "Empty" << endl;
-    
-    if (s.searchMatrix(empty, 0)){
-        cout << "True" << endl;
-    } else{
-        cout << "False" << endl;
-    }
-    
-    cout << "One line" << endl;
-
-    vector<vector<int>> oneLine{
-            {1, 3, 5, 7}
-    };
-
-    if (s.searchMatrix(oneLine, 3)){
-        cout << "True" << endl;
-    } else{
-        cout << "False" << endl;
-    }
-
-    vector<vector<int>> nums {
-            {1,   3,  5,  7},
-            {10, 11, 16, 20},
-            {23, 30, 34, 50},
-            {51, 60, 62, 71},
-    };
-
-    vector<int> test = {3, 11, 34, 2, 12, 31, 0, 90};
-    
-    cout << "Test" << endl;
-    
-    for (int index : test){
-        if (s.searchMatrix(nums, index)) {
-            cout << "True" << endl;
-        } else{
-            cout << "False" << endl;
-        }
-    }
-    
-    vector<vector<int>> nums2{
-            {1},
-            {3},
-    };
-    if (s.searchMatrix(nums2, 2)) {
-        cout << "True" << endl;
-    } else {
-        cout << "False" << endl;
-    }
-    if (s.searchMatrix(nums2, 1)) {
-        cout << "True" << endl;
-    } else {
-        cout << "False" << endl;
-    }
+    run_tc("1 2 R 1 3 L");
+    run_tc("10 20 L 10 30 R 20 40 L 20 60 R");
+    run_tc("10 20 R 20 30 R 30 40 R");
+    run_tc("10 20 L 20 30 L 30 40 L");
     
     return 0;
 }
+
+
 
 
 
